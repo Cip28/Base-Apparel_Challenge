b14c738083a6e6229bbcdb06801c498aae78a818
Cip28 <ciprianbarbu38@yahoo.com>
2021-10-14 11:37:57 +0300

    feat: Create index.html

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const form=document.querySelector('.container form');
+const email=document.querySelector('#email');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let emailValue=email.value;
+    console.log(emailValue);
+    if(!validateEmail(emailValue)){
+        form.classList.add('error');
+    }else{
+        form.classList.remove('error');
+    }
+});
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
